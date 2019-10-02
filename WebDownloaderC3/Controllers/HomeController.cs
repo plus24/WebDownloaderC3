f61d7e069a7d02430c70b4aa9c2785500311d3b0
mehsani <mehsani@users.noreply.github.com>
2019-10-02 17:07:08 +1000

    Add project files.

---
+++ WebDownloaderC3/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebDownloaderC3.Models;
+
+namespace WebDownloaderC3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StartDownload(int irid, string url)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (irid == 9662)
+                    {
+                        Uri uri = new Uri(url);
+                        string filename = System.IO.Path.GetFileName(uri.LocalPath);
+                        System.Diagnostics.Process process = new System.Diagnostics.Process();
+                        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                        startInfo.FileName = "cmd.exe";
+                        startInfo.Arguments = $"/C \"C:\\Program Files (x86)\\Internet Download Manager>idman /d {url} /p C:\\inetpub\\wwwroot\\GalleryServer\\gs\\mediaobjects /f d.mkv /s /q /h /n\"";
+                        process.StartInfo = startInfo;
+                        process.Start();
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ WebDownloaderC3/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    startDownload(data);
+}
+
+$('.uploadfilecontainer').on('click', function () {
+    var url = window.prompt(`URL to Download?`, "");
+    startDownload(url);
+});
+
+function startDownload(data) {
+    var irid = window.prompt(`Download ${data}?`, "Ellie's IR ID!");
+    if (irid == null || irid == "") {
+        alert('Type Ellie\'s ir id!');
+    } else {
+        $.post("Home/StartDownload", { irid: irid, url: data });
+    }
+}
